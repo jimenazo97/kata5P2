34336b41adc9e0868672084e3f1d766d204fc2da
andresjimenez <andresjimenez@10.230.171.51>
2017-11-17 14:30:19 +0000

    kata5 P2 version1

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -5,6 +5,10 @@
  */
 package kata5p1;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -22,7 +26,7 @@ public class Kata5P1 {
      * @throws java.lang.ClassNotFoundException
      * @throws java.sql.SQLException
      */
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
         // TODO code application logic here
         Class.forName("org.sqlite.JDBC");	
         Connection con=DriverManager.getConnection("jdbc:sqlite:KATAS.db");
@@ -36,6 +40,37 @@ public class Kata5P1 {
             System.out.println(res.getInt(1));
             System.out.println(res.getString(2));
         }
+        
+        String query1="create table  if not exists Mail ('id' integer PRIMARY KEY AUTOINCREMENT,'mail' text not null)";
+        st.execute(query1);
+        String emails="emailsfile.txt";
+        muestraContenido(emails);
+    }    
+        
+    /**
+     *
+     * @param emails
+     * @throws FileNotFoundException
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public static void muestraContenido(String emails) throws FileNotFoundException, IOException, SQLException {
+      Connection cone=DriverManager.getConnection("jdbc:sqlite:KATAS.db");
+      String mail;
+      FileReader f = new FileReader(emails);
+        try (BufferedReader b = new BufferedReader(f)) {
+            while((mail = b.readLine())!=null) {
+                String query="insert into Mail(mail) values('" + mail + "')";
+                Statement st1;
+                st1=cone.createStatement();
+                st1.executeUpdate(query);
+            } 
+        }       
+        
+        
+        
+        
     }
+}  
     
-}
+
